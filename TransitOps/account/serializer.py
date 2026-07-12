70416373be500e05a8bf4c566298ffb82b29f710
gondaliyahiren99-commits <gondaliyahiren99@gmail.com>
2026-07-12 12:10:47 +0530

    new update check

--- TransitOps/account/serializer.py
+++ TransitOps/account/serializer.py
@@ -1,5 +1,8 @@
+# pyrefly: ignore [missing-import]
+from TransitOps.account.models import User
 from rest_framework import serializers
 
 # class AccountSerializer(serializers.ModelSerializer):
 #     class Meta:
-        
+#         model=User
+#         fields="__all__"

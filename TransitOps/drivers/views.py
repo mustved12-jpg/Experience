d245f2177951f5f2c5a2b7e8a133ff805a1ebf0a
gondaliyahiren99-commits <gondaliyahiren99@gmail.com>
2026-07-12 17:18:35 +0530

    hiren update.

--- TransitOps/drivers/views.py
+++ TransitOps/drivers/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from drivers.serializer import DriverSerializer
+from .models import Driver
 
 # Create your views here.
-# class DriversApi(generics.ListCreateAPIView):
-#     queryset = Driver.objects.all()
-#     serializer_class = DriverSerializer
+class DriversApi(generics.ListCreateAPIView):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
 
-# class DriverUpdateApi(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Driver.objects.all()
-#     serializer_class = DriverSerializer
+class DriverUpdateApi(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer

348b80f6cfcfd4b83d2b97dc674ddd84fb727def
mustved.jpg <mustved12>
2026-07-12 14:20:55 +0530

    add

--- TransitOps/fuel/models.py
+++ TransitOps/fuel/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
 # Create your models here.
+class FuelLog(models.Model):
+    Trip = models.ForeignKey(Trip,on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    liters = models.DecimalField(max_digits=10, decimal_places=2)
+    cost = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
--- TransitOps/trips/models.py
+++ TransitOps/trips/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from fleet.models import Vehicle, Driver
+
+class Trip(models.Model):
+    all_stetus=[
+        ('DRAFT','DRAFT'),
+        ('DISPATCHED','DISPATCHED'),
+        ('COMPLETED','COMPLETED'),
+        ('CANCELLED','CANCELLED')
+    ]
+    
+    # source = models.CharField(max_length=150)
+    # destination = models.CharField(max_length=150)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    cargo_weight = models.DecimalField(max_digits=10, decimal_places=2)
+    # planned_distance = models.DecimalField(max_digits=10, decimal_places=2)
+    # final_odometer = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+    # fuel_consumed = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    # status = models.CharField(max_length=20,choices=all_stetus)
+    created_at = models.DateTimeField(auto_now_add=True)

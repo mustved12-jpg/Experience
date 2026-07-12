ab7e4cbb3e4ac84d038730f1f3e50e894b4950a7
jenishthummar222 <jenishthummar222@gmail.com>
2026-07-12 12:53:16 +0530

    new update model

--- TransitOps/account/models.py
+++ TransitOps/account/models.py
@@ -6,25 +6,30 @@ import uuid
 # Create your models here.
 
 class User(models.Model):
-    STATUS_CHOICES  = [
-        ('Available','Available'),
-        ('On_Trip','On Trip'),
-        ('In_Shop','In Shop'),
-        ('Retired','Retired')
+    ROLE  = [
+        ('Fleet_Manager','Fleet Manager'),
+        ('Dispatcher','Dispatcher'),
+        ('Safety_Officer','Safety Officer'),
+        ('Financial_Analyst','Financial Analyst')
     ]
     public_id = models.UUIDField(
         default=uuid.uuid4,
         unique=True,
         editable=False
     )
-    registration_number = models.CharField(max_length=20,unique=True)
-    vehicle_model = models.CharField(max_length=50)
-    vehicle_type = models.CharField(max_length=50)
-    capacity = models.DecimalField(max_digits=8,decimal_places=2)
-    odometer = models.PositiveIntegerField()  
-    acquisition_cost = models.DecimalField(max_digits=13,decimal_places=2)  # vehicale byuing  price 
-    status = models.CharField(default='Available',choices=STATUS_CHOICES )
+    
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=10)
+    role = models.CharField(choices=ROLE )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.registration_number
+        return self.username
+
+
+    
+
+  
+
+   
